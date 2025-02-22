8a0f120c6927d7fd4660457cb02207693cb3b3df
Jgone2 <jgoneit@gmail.com>
2025-02-22 09:26:21 +0900

    :tada:: Begin a project

---
+++ src/main/java/com/grimeet/grimeet/GrimeetApplication.java
@@ -0,0 +1,13 @@
+package com.grimeet.grimeet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrimeetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrimeetApplication.class, args);
+	}
+
+}
