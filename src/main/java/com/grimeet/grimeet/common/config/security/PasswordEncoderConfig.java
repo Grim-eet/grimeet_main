23ce54c260197176af6275a9a0e7814dd87b01da
Jgone2 <jgoneit@gmail.com>
2025-03-08 14:57:39 +0900

    :wrench:: add passwordEncoder with salt value to use BCrypt

--- src/main/java/com/grimeet/grimeet/common/config/security/PasswordEncoderConfig.java
+++ src/main/java/com/grimeet/grimeet/common/config/security/PasswordEncoderConfig.java
@@ -1,4 +1,14 @@
 package com.grimeet.grimeet.common.config.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
 public class PasswordEncoderConfig {
+
+  @Bean
+  public BCryptPasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
 }
