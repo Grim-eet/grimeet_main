4a7b1787d28604c42220b1b9b531c491bd4f84c4
Jgone2 <jgoneit@gmail.com>
2025-03-09 07:46:14 +0900

    :sparkles:: add UserDetailsServiceImple for findUserByUsername to use nickname

--- src/main/java/com/grimeet/grimeet/domain/user/service/UserDetailServiceImpl.java
+++ src/main/java/com/grimeet/grimeet/domain/user/service/UserDetailServiceImpl.java
@@ -1,4 +1,40 @@
 package com.grimeet.grimeet.domain.user.service;
 
-public class UserDetailServiceImpl {
+import com.grimeet.grimeet.common.config.oauth.UserPrincipalDetails;
+import com.grimeet.grimeet.domain.user.entity.User;
+import com.grimeet.grimeet.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailServiceImpl implements UserDetailsService {
+
+  private final UserRepository userRepository;
+
+  /**
+   * 사용자 정보를 조회하여 UserDetails 객체를 반환.
+   * @param nickname
+   * @return UserDetails
+   * @throws UsernameNotFoundException
+   */
+  @Override
+  public UserDetails loadUserByUsername(String nickname) throws UsernameNotFoundException {
+    User findUser = findUserByEmailOrThrow(nickname);
+
+    List<GrantedAuthority> authorities = Collections.singletonList((GrantedAuthority) () -> "ROLE_USER");
+    return new UserPrincipalDetails(findUser, authorities);
+  }
+
+  private User findUserByEmailOrThrow(String nickname) {
+    return userRepository.findByNickname(nickname)
+            .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다. nickname: " + nickname));
+  }
 }
