4a82e96bd06c09694a1923b565d7862d61af6669
Jgone2 <jgoneit@gmail.com>
2025-03-08 15:03:29 +0900

    :memo:: add memo about config

--- src/main/java/com/grimeet/grimeet/common/config/security/PasswordEncoderConfig.java
+++ src/main/java/com/grimeet/grimeet/common/config/security/PasswordEncoderConfig.java
@@ -6,7 +6,12 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 public class PasswordEncoderConfig {
-
+  /**
+   * 비밀번호 암호화를 위한 BCryptPasswordEncoder 빈 등록
+   * salt를 이용한 해시 알고리즘으로 암호화 -> 같은 비밀번호라도 다른 해시값을 가짐
+   * @see org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+   * @return BCryptPasswordEncoder
+   */
   @Bean
   public BCryptPasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
