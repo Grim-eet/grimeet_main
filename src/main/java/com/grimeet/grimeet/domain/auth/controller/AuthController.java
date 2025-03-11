7f4e0b10376681569ad86d9fd5a98c7417aa3b5e
Jgone2 <jgoneit@gmail.com>
2025-03-12 08:44:19 +0900

    :sparkles:: feat: add AuthResponseDto and AuthController

--- src/main/java/com/grimeet/grimeet/domain/auth/controller/AuthController.java
+++ src/main/java/com/grimeet/grimeet/domain/auth/controller/AuthController.java
@@ -1,4 +1,18 @@
 package com.grimeet.grimeet.domain.auth.controller;
 
+import com.grimeet.grimeet.domain.auth.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/auth")
 public class AuthController {
+
+  private final AuthService authService;
+
+//  @PostMapping("/login")
+
 }
