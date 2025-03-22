5ed71718b39ad49df6e5303a741242c8a43bc1d2
Jgone2 <jgoneit@gmail.com>
2025-03-22 16:08:21 +0900

    :sparkles:: feat: add  entity of social_account

--- src/main/java/com/grimeet/grimeet/domain/auth/entity/SocialAccount.java
+++ src/main/java/com/grimeet/grimeet/domain/auth/entity/SocialAccount.java
@@ -1,4 +1,38 @@
 package com.grimeet.grimeet.domain.auth.entity;
 
-public class SocialAccount {
+import com.grimeet.grimeet.common.entity.BaseTime;
+import com.grimeet.grimeet.domain.auth.dto.Provider;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Table(name = "social_accounts")
+@Getter
+@NoArgsConstructor
+public class SocialAccount extends BaseTime {
+
+  @Id
+  @GeneratedValue(strategy = IDENTITY)
+  private Long id;
+
+  @Column(name = "social_id", length = 255, nullable = false)
+  private String socialId;
+
+  @Column(name = "provider", nullable = false)
+  @Enumerated(EnumType.STRING)
+  private Provider provider;
+
+  @Column(name = "user_id", nullable = false)
+  private Long userId;
+
+  @Builder
+  public SocialAccount(String socialId, Provider provider, Long userId) {
+    this.socialId = socialId;
+    this.provider = provider;
+    this.userId = userId;
+  }
 }
