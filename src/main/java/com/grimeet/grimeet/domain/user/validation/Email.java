64237d5565c4169ca01946b224d154c67a2ab6ae
mirim <marinrim23@gmail.com>
2025-04-01 19:57:16 +0900

    ✨ Create Email annotation

--- src/main/java/com/grimeet/grimeet/domain/user/validation/Email.java
+++ src/main/java/com/grimeet/grimeet/domain/user/validation/Email.java
@@ -1,5 +1,18 @@
 package com.grimeet.grimeet.domain.user.validation;
 
-public @interface class Email {
+import jakarta.validation.Constraint;
+import org.hibernate.validator.internal.constraintvalidators.bv.EmailValidator;
 
+import java.lang.annotation.*;
+
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = EmailValidator.class)
+@Documented
+public @interface Email {
+
+    String message() default "유효하지 않은 이메일 형식입니다.";
+
+    Class<?>[] groups() default {};
+    Class<?>[] payload() default {};
 }
