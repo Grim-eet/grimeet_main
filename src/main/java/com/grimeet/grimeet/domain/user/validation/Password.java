c5bc6df41083bc6b8870b423f762f98b25861d01
mirim <marinrim23@gmail.com>
2025-04-01 19:42:04 +0900

    ♻️ Delete useless message

--- src/main/java/com/grimeet/grimeet/domain/user/validation/Password.java
+++ src/main/java/com/grimeet/grimeet/domain/user/validation/Password.java
@@ -11,10 +11,7 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 @Documented
 public @interface Password {
 
-    String message() default "비밀번호가 유효하지 않습니다.";
-
-    String patternMessage() default "비밀번호는 영문 대/소문자, 숫자, 특수문자를 각각 하나 이상 포함해야 합니다.";
-    String lengthMessage() default "비밀번호는 {minLength}자 이상 {maxLength}자 이하여야 합니다.";
+    String message() default "비밀번호가 유효하지 않습니다."; // 예외 처리로 삭제 여부 판단
 
     int minLength() default 8;
     int maxLength() default 20;
