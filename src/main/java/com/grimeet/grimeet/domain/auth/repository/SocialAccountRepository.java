ef6dc10d7820b7700776b9bca1da3d24fbdf24f9
Jgone2 <jgoneit@gmail.com>
2025-04-01 20:01:54 +0900

    :sparkles:: feat: add classs SocialAccountRepository and CustomOAuth2UserService for OAtuh login and manage SocialAccount entity

--- src/main/java/com/grimeet/grimeet/domain/auth/repository/SocialAccountRepository.java
+++ src/main/java/com/grimeet/grimeet/domain/auth/repository/SocialAccountRepository.java
@@ -1,4 +1,10 @@
 package com.grimeet.grimeet.domain.auth.repository;
 
-public interface SocialAccountRepository {
+import com.grimeet.grimeet.domain.auth.entity.SocialAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SocialAccountRepository extends JpaRepository<SocialAccount, Long> {
+
 }
--- src/main/java/com/grimeet/grimeet/domain/auth/service/CustomOAuth2UserServiceImpl.java
+++ src/main/java/com/grimeet/grimeet/domain/auth/service/CustomOAuth2UserServiceImpl.java
@@ -1,4 +1,49 @@
 package com.grimeet.grimeet.domain.auth.service;
 
-public class CustomOAuth2UserServiceImpl {
+import com.grimeet.grimeet.common.config.oauth.info.OAuthAttributes;
+import com.grimeet.grimeet.domain.auth.repository.SocialAccountRepository;
+import com.grimeet.grimeet.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.Map;
+
+@Service
+public class CustomOAuth2UserServiceImpl extends DefaultOAuth2UserService {
+  private final UserRepository userRepository;
+  private final SocialAccountRepository socialAccountRepository;
+
+  public CustomOAuth2UserServiceImpl(UserRepository userRepository, SocialAccountRepository socialAccountRepository) {
+    super();
+    this.userRepository = userRepository;
+    this.socialAccountRepository = socialAccountRepository;
+  }
+
+  @Override
+  public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+    OAuth2User oAuth2User = super.loadUser(userRequest);
+    String registrationId = userRequest.getClientRegistration().getRegistrationId();
+    String userNameAttributeName = userRequest.getClientRegistration().getProviderDetails()
+            .getUserInfoEndpoint().getUserNameAttributeName();
+    OAuthAttributes oAuthAttributes = OAuthAttributes.of(registrationId, userNameAttributeName, oAuth2User.getAttributes());
+
+    return new DefaultOAuth2User(
+            Collections.singleton(new SimpleGrantedAuthority("ROLE_USER")),
+            oAuthAttributes.getAttributes(),
+            oAuthAttributes.getNameAttributeKey()
+    );
+  }
+
+  @Override
+  public void setAttributesConverter(Converter<OAuth2UserRequest, Converter<Map<String, Object>, Map<String, Object>>> attributesConverter) {
+    super.setAttributesConverter(attributesConverter);
+  }
 }
