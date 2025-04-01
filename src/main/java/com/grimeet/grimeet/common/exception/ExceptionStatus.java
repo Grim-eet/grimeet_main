0747a5722c0caa19d653531ffb6b93fb53eabaaf
mirim <marinrim23@gmail.com>
2025-04-01 19:23:03 +0900

    ✨ Edit INVALID_PASSWORD in Exception status
    비밀번호가 유효하지 않습니다로 수정하면서 @Password의 에러메세지로도 활용할 수 있게 했다.

--- src/main/java/com/grimeet/grimeet/common/exception/ExceptionStatus.java
+++ src/main/java/com/grimeet/grimeet/common/exception/ExceptionStatus.java
@@ -18,7 +18,7 @@ public enum ExceptionStatus {
   NICKNAME_ALREADY_EXISTS(HttpStatus.BAD_REQUEST, 400, "이미 존재하는 닉네임입니다."),
   PHONE_NUMBER_ALREADY_EXISTS(HttpStatus.BAD_REQUEST, 400, "이미 존재하는 전화번호입니다."),
   INVALID_USER_LOGIN_INFO(HttpStatus.BAD_REQUEST, 400, "아이디 혹은 비밀번호가 일치하지 않습니다."),
-  INVALID_PASSWORD(HttpStatus.BAD_REQUEST, 400, "비밀번호를 재입력하세요."),
+  INVALID_PASSWORD(HttpStatus.BAD_REQUEST, 400, "비밀번호가 유효하지 않습니다."),
   INVALID_ROLE(HttpStatus.FORBIDDEN, 403, "접근할 수 없습니다."),
   INVALID_USER_STATUS(HttpStatus.BAD_REQUEST, 400, "잘못된 유저상태입니다.");
 
