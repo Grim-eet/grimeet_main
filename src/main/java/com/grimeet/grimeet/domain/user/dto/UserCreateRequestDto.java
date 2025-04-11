67aba7f3557127882f0401f3c88c0da64962764d
mirim <marinrim23@gmail.com>
2025-04-11 17:04:40 +0900

    📝 Add swagger doc into user
    UserController를 비롯한 DTO들에서 API 문서 작업 진행

--- src/main/java/com/grimeet/grimeet/domain/user/dto/UserCreateRequestDto.java
+++ src/main/java/com/grimeet/grimeet/domain/user/dto/UserCreateRequestDto.java
@@ -12,14 +12,15 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(description = "신규 사용자 생성 요청 DTO")
 public class UserCreateRequestDto {
 
-  @Schema(description = "사용자 이름", example = "곽두팔")
+  @Schema(description = "사용자 이름", example = "홍길동")
   @NotBlank
   @Size(min = 2, max = 50)
   private String name;
 
-  @Schema(description = "이메일(로그인 아이디)", example = "dopalPrincess98@gmail.com")
+  @Schema(description = "이메일(로그인 아이디)", example = "testUser@example.com")
   @NotBlank
   @Email
   private String email;
@@ -29,7 +30,7 @@ public class UserCreateRequestDto {
   @PasswordFormat
   private String password;
 
-  @Schema(description = "닉네임", example = "zl존두팔S2")
+  @Schema(description = "닉네임", example = "둘리")
   @NotBlank
   @Size(min = 2, max = 50)
   private String nickname;
