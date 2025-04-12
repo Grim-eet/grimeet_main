55dda4cd0c6a20b0c4dfabbab75659c4b796d6fe
mirim <marinrim23@gmail.com>
2025-04-12 18:55:50 +0900

    ✨ Update S3ImageServiceImpl

--- src/main/java/com/grimeet/grimeet/domain/upload/service/S3ImageService.java
+++ src/main/java/com/grimeet/grimeet/domain/upload/service/S3ImageService.java
@@ -2,15 +2,11 @@ package com.grimeet.grimeet.domain.upload.service;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+
 public interface S3ImageService {
 
-    String upload(MultipartFile image);
-
-    String uploadImage(MultipartFile image);
-
-    void validateImageFileExtention(String fileName);
-
-    String uploadImageToS3(MultipartFile image);
+    String upload(MultipartFile image) throws IOException;
 
     void deleteImageFromS3(String imageAddress);
 
--- src/main/java/com/grimeet/grimeet/domain/upload/service/S3ImageServiceImpl.java
+++ src/main/java/com/grimeet/grimeet/domain/upload/service/S3ImageServiceImpl.java
@@ -1,8 +1,26 @@
 package com.grimeet.grimeet.domain.upload.service;
 
 import com.amazonaws.services.s3.AmazonS3;
-import lombok.RequiredArgsConstructor;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.util.IOUtils;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLDecoder;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
 
+import com.grimeet.grimeet.common.exception.ExceptionStatus;
+import com.grimeet.grimeet.common.exception.GrimeetException;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -20,27 +38,84 @@ public class S3ImageServiceImpl implements S3ImageService {
 
 
     @Override
-    public String upload(MultipartFile image) {
-        return "";
+    public String upload(MultipartFile image) throws IOException {
+        if (image.isEmpty() || Objects.isNull(image.getOriginalFilename())) {
+            throw new GrimeetException(ExceptionStatus.INVALID_FILE);
+        }
+
+        this.validateImageFileExtention(image.getOriginalFilename());
+
+        return this.uploadImageToS3(image);
     }
 
-    @Override
-    public String uploadImage(MultipartFile image) {
-        return "";
+    private void validateImageFileExtention(String fileName) {
+        int lastDotIndex = fileName.lastIndexOf(".");
+        if (lastDotIndex == -1) {
+            throw new GrimeetException(ExceptionStatus.INVALID_FILE);
+        }
+
+        String extention = fileName.substring(lastDotIndex + 1).toLowerCase();
+        List<String> allowedExtentionList = Arrays.asList("jpg", "jpeg", "png", "gif");
+
+        if (!allowedExtentionList.contains(extention)) {
+            throw new GrimeetException(ExceptionStatus.INVALID_FILE);
+        }
     }
 
-    @Override
-    public void validateImageFileExtention(String fileName) {
+    public String uploadImageToS3(MultipartFile image) throws IOException{
+        String originalFilename = image.getOriginalFilename(); //원본 파일 명
 
-    }
+        String extention = originalFilename.substring(originalFilename.lastIndexOf(".")); //확장자 명
 
-    @Override
-    public String uploadImageToS3(MultipartFile image) {
-        return "";
+        String s3FileName = UUID.randomUUID().toString().substring(0, 10) + originalFilename; //변경된 파일 명
+
+        InputStream is = image.getInputStream();
+        byte[] bytes = IOUtils.toByteArray(is);
+
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentType("image/" + extention);
+        metadata.setContentLength(bytes.length);
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes);
+
+        try {
+            PutObjectRequest putObjectRequest =
+                    new PutObjectRequest(bucketName, s3FileName, byteArrayInputStream, metadata)
+                            .withCannedAcl(CannedAccessControlList.PublicRead);
+            amazonS3.putObject(putObjectRequest); // put image to S3
+
+        } catch (Exception e){
+            throw new GrimeetException(ExceptionStatus.INVALID_FILE);
+
+        } finally {
+            byteArrayInputStream.close();
+            is.close();
+        }
+
+        return amazonS3.getUrl(bucketName, s3FileName).toString();
     }
 
     @Override
     public void deleteImageFromS3(String imageAddress) {
+        String key = getKeyFromImageAddress(imageAddress);
+        try {
 
+            amazonS3.deleteObject(new DeleteObjectRequest(bucketName, key));
+
+        } catch (Exception e){
+            throw new GrimeetException(ExceptionStatus.INVALID_FILE);
+        }
     }
+
+    private String getKeyFromImageAddress(String imageAddress){
+        try {
+
+            URL url = new URL(imageAddress);
+            String decodingKey = URLDecoder.decode(url.getPath(), "UTF-8");
+            return decodingKey.substring(1); // 맨 앞의 '/' 제거
+
+        } catch (MalformedURLException | UnsupportedEncodingException e){
+            throw new GrimeetException(ExceptionStatus.INVALID_FILE);
+        }
+    }
+
 }
