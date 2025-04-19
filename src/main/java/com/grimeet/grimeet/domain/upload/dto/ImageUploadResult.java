e782e408e388d9921d7da74a07fcc4d788e37e13
mirim <marinrim23@gmail.com>
2025-04-19 17:23:59 +0900

    ✨ Add NoArgsConstructor

--- src/main/java/com/grimeet/grimeet/domain/upload/dto/ImageUploadResult.java
+++ src/main/java/com/grimeet/grimeet/domain/upload/dto/ImageUploadResult.java
@@ -2,8 +2,10 @@ package com.grimeet.grimeet.domain.upload.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class ImageUploadResult {
 
