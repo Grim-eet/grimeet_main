084319b605419af1e38e32b56dd6d6221cf8c9d6
Jgone2 <jgoneit@gmail.com>
2025-04-21 15:57:11 +0900

    :recycle:: refactor: delete temporal annotation and columnDefinition datetime to date

--- src/main/java/com/grimeet/grimeet/domain/userLog/service/UserLogServiceImpl.java
+++ src/main/java/com/grimeet/grimeet/domain/userLog/service/UserLogServiceImpl.java
@@ -10,10 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
-import java.time.ZoneId;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Slf4j
 @Service
