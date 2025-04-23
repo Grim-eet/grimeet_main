cb81a50554e76911d99701023d8e658fac9767ea
mirim <marinrim23@gmail.com>
2025-04-23 14:56:25 +0900

    ✨ Update Redis setting into docker-compose-redis, app

--- src/main/java/com/grimeet/grimeet/domain/auth/service/RedisService.java
+++ src/main/java/com/grimeet/grimeet/domain/auth/service/RedisService.java
@@ -12,14 +12,17 @@ public class RedisService {
 
     private final StringRedisTemplate redisTemplate;
 
+    // Redis에 값 저장, 유효시간 설정
     public void setCode(String key, String value, long expireTime) {
         redisTemplate.opsForValue().set(key, value, expireTime, TimeUnit.MINUTES);
     }
 
+    // Redis에서 값 조회
     public String getCode(String key) {
         return redisTemplate.opsForValue().get(key);
     }
 
+    //Redis에서 값 삭제
     public void deleteCode(String key) {
         redisTemplate.delete(key);
     }
